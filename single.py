813832abb82167ca57e56f8f09f3880fbace75b5
akram-idrissi <akram.mo.gb@gmail.com>
2022-11-23 21:45:31 +0100

    Implemented the base main feature

--- single.py
+++ single.py
@@ -2,6 +2,10 @@ import os
 import pytube
 import sys
 
+MP3 = '.mp3'
+SINGLE_SONGS_DIR = 'single-songs'
+
+
 def validate_args(args):
     if len(args) < 2:
         print('The provided arguments are not enough')
@@ -20,9 +24,23 @@ def get_links(path):
     return links   
 
 
-def download(link):
-    pass
+def to_mp3(audio_mp4):
+    name, _ = os.path.splitext(audio_mp4)
+    audio_mp3 = name + MP3
+    os.rename(os.path.join(SINGLE_SONGS_DIR, audio_mp4), 
+        os.path.join(SINGLE_SONGS_DIR, audio_mp3))
 
+
+def download(link):
+    try:
+        video = pytube.YouTube(link)
+        print(f'Downloading {video.title}')
+        audio = video.streams.filter(only_audio=True).first()
+        audio = audio.download(SINGLE_SONGS_DIR)
+        to_mp3(audio)
+    except Exception as e:
+        print(f'Error {str(e)}')
+    
 
 def main():
 
@@ -31,7 +49,9 @@ def main():
     input = args[1]
 
     links = get_links(input)
-    print(links)
+
+    if not os.path.exists(SINGLE_SONGS_DIR):
+        os.mkdir(SINGLE_SONGS_DIR)
 
     for link in links:
         download(link)
